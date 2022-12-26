606d73a9fdb63439efa155ffcaae01cc212ef8ef
agent068 <121437147+agent068@users.noreply.github.com>
2022-12-26 18:15:59 +0500

    добавлен класс Player

--- pingpon.py
+++ pingpon.py
@@ -5,7 +5,13 @@ win_width = 750
 win_height = 500
 mw = display.set_mode((win_width,win_height))
 mw.fill(back)
-
+class Player():
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 5:
+            self.rect.y += self.speed
 clock = time.Clock()
 game = True
 finish = False
@@ -14,7 +20,10 @@ while game:
         if e.type == QUIT:
             game = False
     if not finish:
+
         display.update()
     clock.tick(40)
+
+
 
 
